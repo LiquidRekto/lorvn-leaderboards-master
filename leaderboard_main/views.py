41d0e153b8ef5c1913c33e15af4fb7c75e2869d8
LiquidRekt <dung6ctd@gmail.com>
2020-05-16 21:59:28 +0700

    fix redirect error

--- leaderboard_main/views.py
+++ leaderboard_main/views.py
@@ -33,5 +33,5 @@ def about(request):
     return render(request, 'leaderboard/about.html')
 
 def auto_redirect(request):
-    response = redirect('/leaderboard/')
+    response = redirect('/leaderboards/')
     return response
