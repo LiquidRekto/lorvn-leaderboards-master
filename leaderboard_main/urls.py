f47f1b4753710447bc9f61e7daef93a0345034e1
LiquidRekt <dung6ctd@gmail.com>
2020-05-21 18:29:20 +0700

    start publishing dev version

--- leaderboard_main/urls.py
+++ leaderboard_main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="leaderboard_main-home"),
-    path('about/', views.about, name="leaderboard_main-about")
+    path('about/', views.about, name="leaderboard_main-about"),
     path('devh2xJVloI9K32/',views.dev, name="leaderboard_main-dev")
 ]
