0c69c00ea83e411bd613d176c16f03de5c74cc9a
LiquidRekt <dung6ctd@gmail.com>
2020-05-21 19:48:14 +0700

    lul

--- django_main/settings.py
+++ django_main/settings.py
@@ -120,12 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'leaderboard_main/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'leaderboard_main/static')
+    os.path.join(BASE_DIR,'static')
 ]
 
 
