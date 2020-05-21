dce9ec1f127c950ffec12a62ff2acf06bbefc3aa
LiquidRekt <dung6ctd@gmail.com>
2020-05-21 19:34:58 +0700

    fix static error

--- django_main/settings.py
+++ django_main/settings.py
@@ -122,11 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'leaderboard_main/staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'leaderboard_main/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
